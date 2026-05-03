3dade3510c7bf5ac5720a1fd18e3b68b2122d370
Arturo R Montesinos <arturormk@controlequis.com>
2026-05-03 11:41:32 +0200

    PowerManager::readBatteryVoltage() returns correct value on M5StickC Plus 2

--- src/system/PowerManager.cpp
+++ src/system/PowerManager.cpp
@@ -3,22 +3,21 @@
 #include <esp_system.h>
 #include <driver/gpio.h>
 
+#define PIN_BATTERY_VOLTAGE 38
+
 void PowerManager::begin() {
     gpio_reset_pin((gpio_num_t)POWER_HOLD_PIN);
     pinMode(POWER_HOLD_PIN, OUTPUT);
     digitalWrite(POWER_HOLD_PIN, HIGH);
     Wire1.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+    analogReadResolution(12);
+    analogSetPinAttenuation(PIN_BATTERY_VOLTAGE, ADC_11db);
 }
 
 float PowerManager::readBatteryVoltage() {
-    Wire1.beginTransmission(AXP192_ADDR);
-    Wire1.write(0x78);
-    Wire1.endTransmission(false);
-    Wire1.requestFrom((uint8_t)AXP192_ADDR, (uint8_t)2);
-    uint8_t hi = Wire1.available() ? Wire1.read() : 0;
-    uint8_t lo = Wire1.available() ? Wire1.read() : 0;
-    uint16_t raw = ((uint16_t)hi << 4) | (lo >> 4);
-    return raw * 1.1f / 1000.0f;
+    int sensorValue = analogRead(PIN_BATTERY_VOLTAGE);
+    float voltage = sensorValue * 3300.0f / 4095.0f / 1000.f * 2.0f;
+    return voltage;
 }
 
 void PowerManager::reboot() {
